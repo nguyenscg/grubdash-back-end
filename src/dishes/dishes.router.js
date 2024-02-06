d48adb4066c6ab703b51331fe3578c9bbb541242
Sophia Nguyen <nguyencsg@gmail.com>
2024-02-06 14:07:25 -0600

    attached create route handler for post requests

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router(); // create new instance of Express router
 const controller = require("./dishes.controller"); // import dishes controller
 
-router.route("/").get(controller.list); // use route handler for get requests
+router.route("/").get(controller.list).post(controller.create); // use route handler for get requests
 
 module.exports = router;
