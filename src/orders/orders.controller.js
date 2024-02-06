41ec9d506a1d4398980a2be6a1a5943f20f8d9e1
Sophia Nguyen <nguyencsg@gmail.com>
2024-02-06 16:15:14 -0600

    added delete function for orders

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -29,7 +29,24 @@ function read(req, res, next) {
     res.json({ data: res.locals.order });
 };
 
+function destroy(req, res, next) {
+    const orderId = req.params.orderId;
+    const foundOrder = orders.find((order) => order.id === Number(orderId));
+    if (foundOrder > -1) {
+        orders.splice(foundOrder, 1);
+        res.sendStatus(204);
+    }
+
+    if (res.locals.order.status !== "pending") {
+        return next({
+            status: 400,
+            message: `An order cannot be deleted unless it is pending` 
+        });
+    }
+}
+
 module.exports = {
     list,
     read: [orderExists, read],
+    delete: [orderExists, destroy],
 }
