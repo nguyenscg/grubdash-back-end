4b27894d9f56bce15c283083be6fbbee4632837d
Sophia Nguyen <nguyencsg@gmail.com>
2024-02-06 13:47:15 -0600

    created router-handler function to get /dishes endpoint

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,14 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+// dishes route
+// handlers and middleware functions to create, read, update, and list dishes. dishes cannot be deleted
+
+// router-handler function --- this will GET /dishes endpoint
+function list(req, res) {
+    res.json({ data: dishes });
+}
+
+module.exports = {
+    list,
+};
