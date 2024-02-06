4698ee6e40473ed7a7845a223a86b19ecdcbc16f
Sophia Nguyen <nguyencsg@gmail.com>
2024-02-06 15:26:44 -0600

    added read function for orders

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -11,6 +11,25 @@ function list(req, res) {
     res.json({ data: orders });
 }
 
+function orderExists(req, res, next) {
+    const { orderId } = req.params;
+    const foundOrder = orders.find((order) => order.id === Number(orderId));
+    
+    if(foundOrder) {
+        res.locals.user = foundOrder;
+        return next();
+    }
+    next({
+        status: 404,
+        message: `Order id not found: ${orderId}`,
+    });
+}
+
+function read(req, res, next) {
+    res.json({ data: res.locals.order });
+};
+
 module.exports = {
     list,
+    read: [orderExists, read],
 }
